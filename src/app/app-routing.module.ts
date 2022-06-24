0a4be688d6009139e110a949483f7d27aff9c965
clem-mal <clement.mallambic@laposte.net>
2022-06-24 11:44:46 +0200

    quete router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { SignupComponent } from './signup/signup.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'user', component: UserComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'userProfil', component: UserProfileComponent},
+];
+
+export { routes};
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
-
+import { MenuComponent } from './menu/menu.component';
+import { UserComponent } from './user/user.component';
+import { SignupComponent } from './signup/signup.component';
+import { routes } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    MenuComponent,
+    UserComponent,
+    SignupComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
